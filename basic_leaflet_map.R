9cb73cc54e00f07941d655173558a26f4897f25b
vasya <trubetskoy.vasa@gmail.com>
2018-04-01 23:21:19 +0200

    Basic leaflet map with R working.

---
+++ basic_leaflet_map.R
@@ -0,0 +1,23 @@
+library(leaflet)
+library(RCurl)
+library(htmlwidgets)
+
+csv_handle <- getURL('https://docs.google.com/spreadsheets/d/e/2PACX-1vQ5PIoUB3bujfpUwi2phvXx_ZecNTpRfTVGc9tq2cgsMQ5uttRxZs86p_cZ_RF9L9FrT8rTxLUNlNwF/pub?gid=939729708&single=true&output=csv')
+sterni <- read.csv(textConnection(csv_handle))
+
+pal <- colorNumeric(
+  palette = 'Blues',
+  domain = sterni$cost)
+
+
+map <- leaflet(data=sterni) %>% 
+  setView(lat = 52.506955, lng = 13.407455, zoom = 12) %>%
+  addProviderTiles('CartoDB.DarkMatterNoLabels') %>%
+  addCircleMarkers(lng = ~lon, lat = ~lat, color = ~pal(cost), stroke = FALSE, radius = 3, fillOpacity = 1) %>%
+  addLegend("bottomright", pal = pal, values = ~cost,
+            title = "cost",
+            opacity = 1)
+
+map
+
+saveWidget(map, file='/Users/vasya/Projects/sterni_index/test_index/index.html', selfcontained=FALSE)
